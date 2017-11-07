ae8efe12c9fa7e7ab1dcececafeb7a13b02f2b85
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-07 13:20:08 +0900

    'ファイルを追加'

--- index.js
+++ index.js
@@ -1,62 +1,71 @@
-require('dotenv').config();
+'use strict';
 
-var builder = require('botbuilder');
-var restify = require('restify');
-
-// auth for connector
-
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
-// making server
-
+var http = require('http');
+var restify = require('restify'); // ローカル開発用のフレームワーク
+var builder = require('botbuilder'); // Bot Builder SDK
 var server = restify.createServer();
-server.listen(process.env.PORT || 3978, function () {
+
+server.listen(3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
+
+var connector = new builder.ChatConnector();
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// making bot
-console.log("hellow!");
+var API_KEY = 'f3502d594b68a566f92d483013bc6aa0';
+var URL = 'http://api.openweathermap.org/data/2.5/weather?q=Tokyo,JP&units=metric&appid=' + API_KEY;
 
-var bot = new builder.UniversalBot(connector);
-bot.dialog('/', function (session) {
-    session.send('hey Hello World');
-});
+// 天気を取得します。
+function getWeather () {
+    return new Promise((resolve, reject) => {
+        http.get(URL, (res) => {
+            let rawData = '';
+            res.on('data', chunk => {
+                rawData += chunk;
+            });
+            res.on('end', () => {
+                resolve(JSON.parse(rawData));
+            });
+        }).on('error', err => {
+            reject(err.message);
+        });
+    });
+}
 
-//higashiyama add
+// ユーザーからの全てのメッセージに反応する、ルートダイアログです。
+bot.dialog('/', [
+    session => {
+        // 下部にあるaskダイアログに会話の制御を渡します。
+        session.beginDialog('/ask');
+    },
+    // askダイアログが閉じられると、制御がルートダイアログに戻り下記が実行されます。
+    (session, results) => {
+        var response = results.response.entity;
+        getWeather().then(
+            data => {
+                if (response === '気温') {
+                    session.send('気温は%s°です！', Math.round(data.main.temp));
+                } else if (response === '気圧') {
+                    session.send('気圧は%shpaです！', data.main.pressure);
+                } else if (response === '湿度') {
+                    session.send('湿度は%s％です！', data.main.humidity);
+                }
+            },
+            err => {
+                session.send('天気を取得できませんでした！！');
+            }
+        );
+    }
+]);
 
-var request = require('request');
-
-// URL
-var baseUrl = 'https://hgsym-iap.demo-mbp.com/imart/logic/api/sample/im-topics-to-log';
-
-request.get(
-    baseUrl, 
-    { form: { key: 'value', hoge: 'huga'} },
-    function (err, res, body) {
-        if (!err && res.statusCode == 200) {
-            var parse_body = qs.parse(body);
-            console.log(body);
-        } else {
-          console.log(body);
-        }   
-    }   
-);
-
-
-//var request = require('request');
-//var options = {
-//  url: 'https://hgsym-iap.demo-mbp.com/imart/logic/api/sample/im-topics-to-log',
-//  json: true
-//};
-
-//request.get(options, function (error, response, body) {
-//  if (!error && response.statusCode == 200) {
-//    console.log(body.name);
-//  } else {
-//    console.log('error: '+ response.statusCode);
-//  }
-//});
+// askダイアログ
+bot.dialog('/ask', [
+    session => {
+        builder.Prompts.choice(session, "こんにちは！何が知りたいですか?", "気温|気圧|湿度");
+    },
+    (session, results) => {
+        // askダイアログを閉じ、ルートダイアログにユーザーからの返答データを渡します。
+        session.endDialogWithResult(results);
+    }
+]);
