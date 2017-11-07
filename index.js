36655b38b08094b2aefee8160caf7d5f198c3a29
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-07 11:07:41 +0900

    'ファイルを追加'

--- index.js
+++ index.js
@@ -24,3 +24,22 @@ var bot = new builder.UniversalBot(connector);
 bot.dialog('/', function (session) {
     session.send('hey Hello World');
 });
+
+//higashiyama add
+var request = require('request');
+
+// URL
+var baseUrl = 'https://hgsym-iap.demo-mbp.com/imart/logic/api/sample/im-topics-to-log';
+
+request.post(
+    {url: baseUrl, json:true},
+    { form: { key: 'value', hoge: 'huga'} },
+    function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+            var parse_body = qs.parse(body);
+            console.log(body);
+        } else {
+          console.log(body);
+        }   
+    }   
+);
