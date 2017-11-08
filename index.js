c4f421b781eee049ea1e0062224f9391e31b4683
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-08 13:51:02 +0900

    'add'

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ var API_KEY = 'f3502d594b68a566f92d483013bc6aa0';
 // intra-martニュースを取得します。
 function getWeather (message) {
 	var message = encodeURI(message);
-	var URL='http://ec2-13-115-215-14.ap-northeast-1.compute.amazonaws.com/imart/logic/api/sample/accounts?user_cd=' + message;
+//	var URL='http://ec2-13-115-215-14.ap-northeast-1.compute.amazonaws.com/imart/logic/api/sample/accounts?user_cd=' + message;
+	var URL='https://ec2-13-115-215-14.ap-northeast-1.compute.amazonaws.com/imart/logic/api/sample/accounts?user_cd=' + message;
     return new Promise((resolve, reject) => {
 //        http.get(URL, (res) => {
         https.get(URL, (res) => {
