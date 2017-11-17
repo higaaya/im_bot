a0ebc54195dc1381ff1bf75b0a744f8adabc1bb3
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-17 14:35:21 +0900

    'add'

--- index.js
+++ index.js
@@ -2,25 +2,26 @@
 
 var builder = require('botbuilder'); // Bot Builder SDK
 var restify = require('restify'); // ローカル開発用のフレームワーク
-var https = require('https');
+var http = require('http');
+
 var connector = new builder.ChatConnector({
 	appId: process.env.MICROSOFT_APP_ID,
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var server = restify.createServer();
 server.listen(process.env.PORT || 3978, function () {
-	console.log('%s listening to %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url);
 });
 server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
 
-var URL = 'https://hgsym-iap.demo-mbp.com/imart/logic/api/sample/im-topics-to-log';
+var API_KEY = 'f3502d594b68a566f92d483013bc6aa0';
+var URL = 'http://api.openweathermap.org/data/2.5/weather?q=Tokyo,JP&units=metric&appid=' + API_KEY;
 
-// intra-martニュースを取得します。
+// 天気を取得します。
 function getWeather () {
-    return new Promise((resolve, reject) => {
-		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; //https:自己証明書のとき必要？？
-		https.get(URL, (res) => {
+	return new Promise((resolve, reject) => {
+		http.get(URL, (res) => {
 			let rawData = '';
 			res.on('data', chunk => {
 				rawData += chunk;
@@ -45,16 +46,16 @@ bot.dialog('/', [
 		var response = results.response.entity;
 		getWeather().then(
 			data => {
-				if (response === 'タイトル') {
-					session.send('タイトルは%sです！', data.topics[0].title);
-				} else if (response === 'リンク') {
-					session.send('リンクは%sです！', data.topics[0].link);
-				} else if (response === '日付') {
-					session.send('日付は%sです！', data.topics[0].published);
+				if (response === '気温') {
+					session.send('気温は%s°です！', Math.round(data.main.temp));
+				} else if (response === '気圧') {
+					session.send('気圧は%shpaです！', data.main.pressure);
+				} else if (response === '湿度') {
+					session.send('湿度は%s％です！', data.main.humidity);
 				}
 			},
 			err => {
-				session.send('intra-martニュースを取得できませんでした！！');
+				session.send('天気を取得できませんでした！！');
 			}
 		);
 	}
@@ -63,7 +64,7 @@ bot.dialog('/', [
 // askダイアログ
 bot.dialog('/ask', [
 	session => {
-		builder.Prompts.choice(session, "こんにちは！intra-mart最新ニュースの何が知りたいですか?", "タイトル|リンク|日付");
+		builder.Prompts.choice(session, "こんにちは！何が知りたいですか?", "気温|気圧|湿度");
 	},
 	(session, results) => {
 		// askダイアログを閉じ、ルートダイアログにユーザーからの返答データを渡します。
