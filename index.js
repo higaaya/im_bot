e7a1559a20b044d8adf10b5713427585cadf0ca9
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-17 14:07:38 +0900

    'add'

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 
 var builder = require('botbuilder');
 var restify = require('restify');
-var http = require('http');
-var https = require('https');
-
+var request = require('request');
 var connector = new builder.ChatConnector({
 	appId: process.env.MICROSOFT_APP_ID,
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -16,27 +14,47 @@ server.listen(process.env.PORT || 3978, function () {
 server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
 
+// 未処理一覧を取得します。
+function getUnprocessActvMatterNodeList (message) {
 
-// ユーザーアカウント情報を取得します。
-function getUserAccount (message) {
-	var message = encodeURI(message);
-	var URL='https://hgsym-iap.demo-mbp.com/imart/logic/api/sample/accounts?user_cd=' + message;
-//	var URL='http://localhost:8081/imart/logic/api/sample/accounts?user_cd=' + message;
-	return new Promise((resolve, reject) => {
+	let options = {
+//		url: 'http://localhost:8081/imart/api/forma/imw/selectUnprocessActvMatterNodeList',
+		url: 'https://hgsym-iap.demo-mbp.com/imart/api/forma/imw/selectUnprocessActvMatterNodeList',
+		headers: {
+			'Content-Type':'application/json'
+		},
+		json: true,
+  		body: {
+			'processAuthCondition': {
+				'applyFlag': true,
+				'approveFlag': true,
+				'applyActFlag': true,
+				'approveActFlag': true
+			},
+			'unprocessActvMatterNodeSearchCondition': {
+				'baseUserCode': message,
+				'listSearchCondition': {
+					'offset': 0,
+					'count': 0,
+					'andCombinationFlag': true
+				}
+			}
+		}
+	};
+
+    return new Promise((resolve, reject) => {
 		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; //https:自己証明書のとき必要？？
-		https.get(URL, (res) => {
-//		http.get(URL, (res) => {
-			let rawData = '';
-			res.on('data', chunk => {
-				rawData += chunk;
-			});
-			res.on('end', () => {
-				resolve(JSON.parse(rawData));
-			});
-		}).on('error', err => {
-			reject(err.message);
+		request.post(options, function(error, response, body){
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+				resolve(body);
+			} else {
+				console.log('error: '+ response.statusCode);
+				resolve(error);
+			}
 		});
-	});
+    });
+
 }
 
 // ユーザーからの全てのメッセージに反応する、ルートダイアログです。
@@ -48,9 +66,23 @@ bot.dialog('/', [
 	// askダイアログが閉じられると、制御がルートダイアログに戻り下記が実行されます。
 	(session, results) => {
 		var response = results.response;
-		getUserAccount(response).then(
+		getUnprocessActvMatterNodeList(response).then(
 			data => {
-				session.send('%s回です！', data.records[0].login_failure_count);
+				var misoyri = '';
+				for (var item in data) {
+					for (var subItem in data[item]) {
+						misoyri = misoyri + '--------------------------------------------------\n\n';
+						misoyri = misoyri + '案件名：' + data[item][subItem]['matterName'] + '\n\n';
+						misoyri = misoyri + '申請者：' + data[item][subItem]['applyExecuteUserName'] + '\n\n';
+						misoyri = misoyri + '申請日：' + data[item][subItem]['applyDate'] + '\n\n';
+						misoyri = misoyri + 'ステータス：' + data[item][subItem]['status'] + '\n\n';
+						misoyri = misoyri + '--------------------------------------------------\n\n\n\n';
+					}
+				}
+				if(misoyri == ''){
+					misoyri = '0件です。';
+				}
+				session.send('%s', misoyri);
 			},
 			err => {
 				session.send('%s', err);
@@ -62,7 +94,7 @@ bot.dialog('/', [
 // askダイアログ
 bot.dialog('/ask', [
 	session => {
-		builder.Prompts.text(session, "こんにちは！どのユーザーのログイン失敗回数が知りたいですか？");
+		builder.Prompts.text(session, "こんにちは！未処理案件の一覧を表示したいユーザーを教えてください");
 	},
 	(session, results) => {
 		// askダイアログを閉じ、ルートダイアログにユーザーからの返答データを渡します。
