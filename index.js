a7e2b6043a4e250006411c8f589eff498c7be5a3
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-17 14:11:35 +0900

    'add'

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+'use strict';
 
-var builder = require('botbuilder');
-var restify = require('restify');
-var request = require('request');
+var builder = require('botbuilder'); // Bot Builder SDK
+var restify = require('restify'); // ローカル開発用のフレームワーク
+var http = require('http');
 var connector = new builder.ChatConnector({
 	appId: process.env.MICROSOFT_APP_ID,
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -14,47 +14,23 @@ server.listen(process.env.PORT || 3978, function () {
 server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
 
-// 未処理一覧を取得します。
-function getUnprocessActvMatterNodeList (message) {
-
-	let options = {
-//		url: 'http://localhost:8081/imart/api/forma/imw/selectUnprocessActvMatterNodeList',
-		url: 'https://hgsym-iap.demo-mbp.com/imart/api/forma/imw/selectUnprocessActvMatterNodeList',
-		headers: {
-			'Content-Type':'application/json'
-		},
-		json: true,
-  		body: {
-			'processAuthCondition': {
-				'applyFlag': true,
-				'approveFlag': true,
-				'applyActFlag': true,
-				'approveActFlag': true
-			},
-			'unprocessActvMatterNodeSearchCondition': {
-				'baseUserCode': message,
-				'listSearchCondition': {
-					'offset': 0,
-					'count': 0,
-					'andCombinationFlag': true
-				}
-			}
-		}
-	};
-
-    return new Promise((resolve, reject) => {
-		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; //https:自己証明書のとき必要？？
-		request.post(options, function(error, response, body){
-			if (!error && response.statusCode == 200) {
-				console.log(body);
-				resolve(body);
-			} else {
-				console.log('error: '+ response.statusCode);
-				resolve(error);
-			}
+// メールを送信します。
+function sendMail (message) {
+	var message = encodeURI(message);
+	var URL='http://hgsym-iap.demo-mbp.com/imart/logic/api/tutorial/flow?message=' + message;
+	return new Promise((resolve, reject) => {
+		http.get(URL, (res) => {
+			let rawData = '';
+			res.on('data', chunk => {
+				rawData += chunk;
+			});
+			res.on('end', () => {
+				resolve(JSON.parse(rawData));
+			});
+		}).on('error', err => {
+			reject(err.message);
 		});
-    });
-
+	});
 }
 
 // ユーザーからの全てのメッセージに反応する、ルートダイアログです。
@@ -66,23 +42,9 @@ bot.dialog('/', [
 	// askダイアログが閉じられると、制御がルートダイアログに戻り下記が実行されます。
 	(session, results) => {
 		var response = results.response;
-		getUnprocessActvMatterNodeList(response).then(
+		sendMail(response).then(
 			data => {
-				var misoyri = '';
-				for (var item in data) {
-					for (var subItem in data[item]) {
-						misoyri = misoyri + '--------------------------------------------------\n\n';
-						misoyri = misoyri + '案件名：' + data[item][subItem]['matterName'] + '\n\n';
-						misoyri = misoyri + '申請者：' + data[item][subItem]['applyExecuteUserName'] + '\n\n';
-						misoyri = misoyri + '申請日：' + data[item][subItem]['applyDate'] + '\n\n';
-						misoyri = misoyri + 'ステータス：' + data[item][subItem]['status'] + '\n\n';
-						misoyri = misoyri + '--------------------------------------------------\n\n\n\n';
-					}
-				}
-				if(misoyri == ''){
-					misoyri = '0件です。';
-				}
-				session.send('%s', misoyri);
+				session.send('(「%s」を送信しました。', response);
 			},
 			err => {
 				session.send('%s', err);
@@ -94,7 +56,7 @@ bot.dialog('/', [
 // askダイアログ
 bot.dialog('/ask', [
 	session => {
-		builder.Prompts.text(session, "こんにちは！未処理案件の一覧を表示したいユーザーを教えてください");
+		builder.Prompts.text(session, "こんにちは！どのような内容をメール送信しますか？");
 	},
 	(session, results) => {
 		// askダイアログを閉じ、ルートダイアログにユーザーからの返答データを渡します。
