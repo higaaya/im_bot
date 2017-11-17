06e5d1402c529ee0c7b1effefb09443cce4b3acb
東山 綾子 <higashiyama_ayako@mbpjp.com>
2017-11-17 14:37:37 +0900

    'add'

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-'use strict';
+require('dotenv').config();
 
-var builder = require('botbuilder'); // Bot Builder SDK
-var restify = require('restify'); // ローカル開発用のフレームワーク
-var http = require('http');
-
+var builder = require('botbuilder');
+var restify = require('restify');
+var request = require('request');
 var connector = new builder.ChatConnector({
 	appId: process.env.MICROSOFT_APP_ID,
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -15,24 +14,47 @@ server.listen(process.env.PORT || 3978, function () {
 server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
 
-var API_KEY = 'f3502d594b68a566f92d483013bc6aa0';
-var URL = 'http://api.openweathermap.org/data/2.5/weather?q=Tokyo,JP&units=metric&appid=' + API_KEY;
+// 未処理一覧を取得します。
+function getUnprocessActvMatterNodeList (message) {
 
-// 天気を取得します。
-function getWeather () {
-	return new Promise((resolve, reject) => {
-		http.get(URL, (res) => {
-			let rawData = '';
-			res.on('data', chunk => {
-				rawData += chunk;
-			});
-			res.on('end', () => {
-				resolve(JSON.parse(rawData));
-			});
-		}).on('error', err => {
-			reject(err.message);
+	let options = {
+//		url: 'http://localhost:8081/imart/api/forma/imw/selectUnprocessActvMatterNodeList',
+		url: 'https://hgsym-iap.demo-mbp.com/imart/api/forma/imw/selectUnprocessActvMatterNodeList',
+		headers: {
+			'Content-Type':'application/json'
+		},
+		json: true,
+  		body: {
+			'processAuthCondition': {
+				'applyFlag': true,
+				'approveFlag': true,
+				'applyActFlag': true,
+				'approveActFlag': true
+			},
+			'unprocessActvMatterNodeSearchCondition': {
+				'baseUserCode': message,
+				'listSearchCondition': {
+					'offset': 0,
+					'count': 0,
+					'andCombinationFlag': true
+				}
+			}
+		}
+	};
+
+    return new Promise((resolve, reject) => {
+		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; //https:自己証明書のとき必要？？
+		request.post(options, function(error, response, body){
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+				resolve(body);
+			} else {
+				console.log('error: '+ response.statusCode);
+				resolve(error);
+			}
 		});
-	});
+    });
+
 }
 
 // ユーザーからの全てのメッセージに反応する、ルートダイアログです。
@@ -43,19 +65,27 @@ bot.dialog('/', [
 	},
 	// askダイアログが閉じられると、制御がルートダイアログに戻り下記が実行されます。
 	(session, results) => {
-		var response = results.response.entity;
-		getWeather().then(
+		var response = results.response;
+		getUnprocessActvMatterNodeList(response).then(
 			data => {
-				if (response === '気温') {
-					session.send('気温は%s°です！', Math.round(data.main.temp));
-				} else if (response === '気圧') {
-					session.send('気圧は%shpaです！', data.main.pressure);
-				} else if (response === '湿度') {
-					session.send('湿度は%s％です！', data.main.humidity);
+				var misoyri = '';
+				for (var item in data) {
+					for (var subItem in data[item]) {
+						misoyri = misoyri + '--------------------------------------------------\n\n';
+						misoyri = misoyri + '案件名：' + data[item][subItem]['matterName'] + '\n\n';
+						misoyri = misoyri + '申請者：' + data[item][subItem]['applyExecuteUserName'] + '\n\n';
+						misoyri = misoyri + '申請日：' + data[item][subItem]['applyDate'] + '\n\n';
+						misoyri = misoyri + 'ステータス：' + data[item][subItem]['status'] + '\n\n';
+						misoyri = misoyri + '--------------------------------------------------\n\n\n\n';
+					}
 				}
+				if(misoyri == ''){
+					misoyri = '0件です。';
+				}
+				session.send('%s', misoyri);
 			},
 			err => {
-				session.send('天気を取得できませんでした！！');
+				session.send('%s', err);
 			}
 		);
 	}
@@ -64,7 +94,7 @@ bot.dialog('/', [
 // askダイアログ
 bot.dialog('/ask', [
 	session => {
-		builder.Prompts.choice(session, "こんにちは！何が知りたいですか?", "気温|気圧|湿度");
+		builder.Prompts.text(session, "こんにちは！未処理案件の一覧を表示したいユーザーを教えてください");
 	},
 	(session, results) => {
 		// askダイアログを閉じ、ルートダイアログにユーザーからの返答データを渡します。
